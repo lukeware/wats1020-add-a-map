50d4752fbd70f97be25154abff02d654be1cbb8d
warel <warel@seattleu.edu>
2016-05-01 15:30:07 -0400

    finished assignment tasks

--- js/main.js
+++ js/main.js
@@ -1,21 +1,75 @@
-/* Begin by adding your on ready handler here, and then create the
-   rest of your functions inside the on ready handler.
+// *** WATS 1020 ADD A MAP *** //
 
-   (Note that you do not need to manually call Bootstrap functions in
-   your Javascript because Bootstrap will automatically recognize your
-   HTML structures and invoke the proper JS code accordingly. Be sure
-   to reference the Bootstrap documentation.)
-*/
+//Set script to run after document load
+$(document).ready(function() {
+    
+    
+    //Draw a map in map-container
+    var map = L.map('map-container').setView([46.843, -121.670], 11);
+    
+	var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+    
+    
+    //Add map layers 
+    var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 18, attribution: osmAttrib});
 
-// TODO: Inside of your on ready handler, invoke the Leaflet.js library
-// to draw a map in your `#map-container` div.
+    var satLayer = L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap"   target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a     href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img  src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
+        subdomains: ['otile1','otile2','otile3','otile4']
+});
 
-// TODO: Add 2 layers to your map you have visuals. Use the Open Street Maps
-// tiles served through the MapQuest CDN. Consult this example to set up
-// the map tiles layers:
+    var drawLayer = L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap"   target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a     href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img  src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
+        subdomains: ['otile1','otile2','otile3','otile4']
+});
+
+    var mapLayers = {
+        "Satellite": satLayer,
+        "Map View": drawLayer,
+        "Open Street Maps": osm
+};
+
+    L.control.layers(mapLayers).addTo(map);
+        satLayer.addTo(map);
+    
+
+    //show markers with popups at no fewer than 3 interesting locations.
+    var marker = L.marker([46.852, -121.760]).addTo(map);
+        marker.bindPopup('<b>Welcome to Mt. Rainier!</b><br>This peak is 14,417 feet (4,394 m) high.<br><a href="http://visitrainier.com">Visit Rainier!</a>');
+    
+    var marker = L.marker([46.845757, -121.767565]).addTo(map);
+        marker.bindPopup('<b>Point Success</b><br>Elevation: 14,164 feet (4,300 m)');
+    
+    var marker = L.marker([46.869047, -121.517128]).addTo(map);
+        marker.bindPopup('<b>Tipsoo Lake</b><br>Elevation: 5,290 ft (1,612 m)');
+    
+    var marker = L.marker([46.785836, -121.735372]).addTo(map);
+        marker.bindPopup('<b>Paradise</b><br>Elevation: 5,400 ft (1,645 m)');
+ 
+    var circle = L.circle([46.927725, -121.493720], 1000, {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        }).addTo(map);
+        circle.bindPopup('<b>Crystal Mountain Ski Resort</b>');
+  
+	
+	//Stretch: Clicks on map return lat/long 
+	var popup = L.popup();
+
+	function onMapClick(e) {
+    	popup
+        	.setLatLng(e.latlng)
+        	.setContent("You clicked the map at" + e.latlng.toString())
+        	.openOn(map);
+	}
+
+	map.on('click', onMapClick);
+	
+ });
+    
 
 
-// TODO: Customize that Map to show markers with popups at no fewer than 3
-// interesting locations. (You'll need to figure out the latitude/longitude for
-// these locations using a mapping tool such as Google Maps.)
+
 
